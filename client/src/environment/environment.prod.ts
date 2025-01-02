d58045f79c9db61751b90c98d30ba4acee5eb69c
ANDRIAMIHAJA Joeson Emile <122634018+emilejoelson@users.noreply.github.com>
2025-01-02 21:41:40 +0100

    fix : update env

--- client/src/environment/environment.prod.ts
+++ client/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com',
+  apiUrl: 'http://13.60.211.177/api',
   recaptcha: {
     siteKey: '6LfTgHopAAAAAOvpRU6IOfg59sefC5uRU9x-iTiE',
   },
--- client/src/environment/environment.ts
+++ client/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'http://13.60.211.177/api',
   recaptcha: {
     siteKey: '6LfTgHopAAAAAOvpRU6IOfg59sefC5uRU9x-iTiE',
   },
