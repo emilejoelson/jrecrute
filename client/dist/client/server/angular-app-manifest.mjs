37b47acf5bf1d3a2331cf99472fe5df3a22f1b0e
ANDRIAMIHAJA Joeson Emile <122634018+emilejoelson@users.noreply.github.com>
2025-01-03 15:24:51 +0100

    fix : update logic of form

--- client/dist/client/server/angular-app-manifest.mjs
+++ client/dist/client/server/angular-app-manifest.mjs
@@ -19,11 +19,11 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 9290, hash: '593413eb849a4072c765997c537e1370173920ff5663dfcd61a75d9a570306e5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7645, hash: '82e446a0f5e8a274911055fa8f83d0812dc9f8541251f517c02448109bb9b32f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'deposer-un-cv-avec-succes/index.html': {size: 31794, hash: '714a70bc111912ce42f0b7e86887b63a127f75850a6050e4fe309ee55487616c', text: () => import('./assets-chunks/deposer-un-cv-avec-succes_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 60994, hash: 'fed6962c63bc89bdcc481c7d11d7372c2a4217b68a83a8fe6f89ac5a4f110e70', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'deposer-un-cv/index.html': {size: 33773, hash: '57bdbe794a25be4f91359763138db1f5b8850e15be2ff527dcddff63a1f86896', text: () => import('./assets-chunks/deposer-un-cv_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9290, hash: '81d4145a9a5f3239b8393ee721ffb6e0a664f073d8dfa55f0618675ea73af67f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7645, hash: 'f2fd1c8998d28d4cf32d27c3115f9e61cfdd5bd7b567a1f58c53d130079e6951', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'deposer-un-cv-avec-succes/index.html': {size: 31794, hash: '7ad92acd0486feeec84b4768df5987e265294b86f3297842dac3cea63d22c659', text: () => import('./assets-chunks/deposer-un-cv-avec-succes_index_html.mjs').then(m => m.default)},
+    'deposer-un-cv/index.html': {size: 33773, hash: '5dd35cae950d1ad6ebc0eb106ef11b8f1d389088a28b5120e25b23b932769fd4', text: () => import('./assets-chunks/deposer-un-cv_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 60994, hash: 'b27f2db88669692d5273e698d99a2c2e6feee4c64688c0b76b4c1f1a857ebcaa', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-2EAGLGPP.css': {size: 81924, hash: '9iMF757PQIk', text: () => import('./assets-chunks/styles-2EAGLGPP_css.mjs').then(m => m.default)}
   },
 };
