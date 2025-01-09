e1b22b341e2145cae877bdedc05af93a9a74d6dd
ANDRIAMIHAJA Joeson Emile <122634018+emilejoelson@users.noreply.github.com>
2025-01-09 12:09:39 +0100

    fix : update the css in home page

--- client/dist/client/server/angular-app-manifest.mjs
+++ client/dist/client/server/angular-app-manifest.mjs
@@ -31,14 +31,14 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 9443, hash: 'cb1782a5c6d16836c140557b6b68ae1b3b9d1bf6bf86fa093c3ca812ed6ce7b4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7798, hash: 'bc02f5c885cf26f1eb714779c5ef0d45cdfba6136bdf0c76765dd0e90d05c922', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'client/index.html': {size: 51461, hash: 'b7ae835e4bca87f973a74fc46058319e921a53dac0b771340a197ea11e7e31c6', text: () => import('./assets-chunks/client_index_html.mjs').then(m => m.default)},
-    'deposer-un-cv/index.html': {size: 33655, hash: '9a5198e155a0087a2d426fa550e0a847b5e124a5f62c58f90b84e396d9502c2a', text: () => import('./assets-chunks/deposer-un-cv_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 66868, hash: '4ed0cc8ce3a94b649fb044cf05441fcb32e813f09a7b47f71c531f3e8c01b1cb', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'attente-de-reponse/index.html': {size: 33104, hash: '723018314f6697a0e20a828a515a3eb757f269cc19472b4da641ef4f5e247582', text: () => import('./assets-chunks/attente-de-reponse_index_html.mjs').then(m => m.default)},
-    'mention-legal/index.html': {size: 37486, hash: 'a2a9b807c1439dba73809d4b07070d73e852123ed5a3b1cc70a8a947dc1f4644', text: () => import('./assets-chunks/mention-legal_index_html.mjs').then(m => m.default)},
-    'deposer-un-cv-avec-succes/index.html': {size: 31843, hash: 'b3b4a854b2386805653aad9f454d2719b10bf646458f840f9fc77c6dbe7acd47', text: () => import('./assets-chunks/deposer-un-cv-avec-succes_index_html.mjs').then(m => m.default)},
-    'styles-EDUSYTBE.css': {size: 90416, hash: 'Af32WouqCho', text: () => import('./assets-chunks/styles-EDUSYTBE_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 9443, hash: 'cad260e8de353477411622deb33709639cfad2824811e57b6dec59d732c7f22a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7798, hash: 'e30e36e33713c5aa014e7b6f941ff251500755bfaf2119613305506ed2256839', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 66889, hash: '561c1425e84f6f462885b3928b6b22d4937619c40109183f906e227ac16f3086', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'mention-legal/index.html': {size: 37486, hash: '64025791ade6b693976c2d4483113f86f6eae4bde5a764373ce1b7ceb6e6dfe0', text: () => import('./assets-chunks/mention-legal_index_html.mjs').then(m => m.default)},
+    'attente-de-reponse/index.html': {size: 33104, hash: 'c097132b1da49218d7dcc7e6fd8cc0b59fc1e646ec3ffadc2b3daaf702f3b67c', text: () => import('./assets-chunks/attente-de-reponse_index_html.mjs').then(m => m.default)},
+    'client/index.html': {size: 51461, hash: '6036fcef14fcf5bb71e472dcf284d2a533830752fc93fd9fd19cbbca26da30e7', text: () => import('./assets-chunks/client_index_html.mjs').then(m => m.default)},
+    'deposer-un-cv-avec-succes/index.html': {size: 31843, hash: '17c4c9510bd6d89cab84b1c1bb7e2eeb9fab00ecfdc1f7ccb2220ceb0711fdf1', text: () => import('./assets-chunks/deposer-un-cv-avec-succes_index_html.mjs').then(m => m.default)},
+    'deposer-un-cv/index.html': {size: 33655, hash: 'bedc2c45995141f90aed4ca269d9750ddb909c450118203b7b59a0149aa78ded', text: () => import('./assets-chunks/deposer-un-cv_index_html.mjs').then(m => m.default)},
+    'styles-Y2MQ26VG.css': {size: 90461, hash: 'n4k9ETOfSbo', text: () => import('./assets-chunks/styles-Y2MQ26VG_css.mjs').then(m => m.default)}
   },
 };
