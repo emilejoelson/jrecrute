8ddbc5905b63aa577756753dff0b0a00efc95bf1
ANDRIAMIHAJA Joeson Emile <122634018+emilejoelson@users.noreply.github.com>
2025-01-11 12:22:30 +0100

    fix : udapte dist

---
+++ client/dist/client/browser/chunk-U6LUS6TJ.js
@@ -0,0 +1,1 @@
+import{c as r,d as o}from"./chunk-KHGMYX6W.js";var m=o({source:"User Form API",events:{"Submit user form":r(),"Submit user form success":r(),"Submit user form failure":r()}});export{m as a};
---
+++ client/dist/client/browser/chunk-V43NZ7HU.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KKUI33N7.js";import"./chunk-M2W72IZX.js";export{a as LoadingSpinnerComponent};
